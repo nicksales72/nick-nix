3be389249a7cffff564c39b5c385679dee6bb1d3
nicksales7 <nicksales72@gmail.com>
2024-12-17 19:51:10 -0500

    Updated 2024-12-17

--- hosts/nick/user.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }: {
-  users.users.nick = {
-    isNormalUser = true;
-    shell = "${pkgs.nushell}/bin/nu";
-    extraGroups = [ "wheel" "networkmanager" "docker" ]; 
-    packages = with pkgs; [ tree wget ];
-  };
-}
-
