ba6e30652466630696646f0347a2622a81637ad2
nicksales7 <nicksales72@gmail.com>
2024-12-19 19:29:23 -0500

    Updated 2024-12-19

--- modules/terminals-shells/nushell.nix
+++ modules/terminals-shells/nushell.nix
@@ -1,6 +1,12 @@
-{ pkgs, config, ... }: {
-  programs.nushell = {
-    enable = true;
+{ pkgs, config, lib, ... }: {
+  programs = {
+    nushell = {
+      enable = true;
+      extraConfig = ''
+        $env.PATH = ($env.PATH | 
+        split row (char esep) | 
+        append "/home/nick/.nix-profile/bin")
+        '';
+    };
   };
 }
-
