e5436230409ef9c3bf08765851d1113cc094d2dc
nicksales7 <nicksales72@gmail.com>
2024-12-29 09:53:58 -0500

    Updated 2024-12-29

--- modules/themes/gtk-settings.nix
+++ modules/themes/gtk-settings.nix
@@ -1,15 +1,9 @@
 { config, lib, pkgs, ... }: {
-  home.packages = with pkgs; [
-    gnome-themes-extra
-  ];
-
-  xdg.configFile."gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name = Adwaita-dark
-  '';
-
-  xdg.configFile."gtk-4.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name = Adwaita-dark
-  '';
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "SolArc-Dark";
+      package = pkgs.solarc-gtk-theme;
+    };
+  };
 }
