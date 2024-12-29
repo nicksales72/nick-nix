553410dcb86b3bc6fc71b1d2947f21ce72711ed5
nicksales7 <nicksales72@gmail.com>
2024-12-29 11:08:09 -0500

    Updated 2024-12-29

--- modules/themes/gtk-settings.nix
+++ modules/themes/gtk-settings.nix
@@ -1,10 +1,12 @@
 { config, lib, pkgs, ... }: {
+  home.activation = {
+    libadwaita-dark-theme = lib.mkAfter ''
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+    '';
+  };
+
   home.file = {
     ".config/gtk-3.0/settings.ini".text = ''
-      [Settings]
-      gtk-application-prefer-dark-theme = 1
-    '';
-    ".config/gtk-4.0/settings.ini".text = ''
       [Settings]
       gtk-application-prefer-dark-theme = 1
     '';
