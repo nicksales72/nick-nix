0609c1087d6c098e31070b181a782a9301301bd6
nicksales7 <nicksales72@gmail.com>
2025-01-15 10:12:45 -0500

    Updated 2025-01-15

---
+++ modules/editors/helix.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }: {
+  home.packages = with pkgs; [
+    marksman
+  ];
+  
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {        
+      theme = "kanagawa-dragon";
+      editor = {
+        line-number = "relative";
+        mouse = false;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+      };
+    };
+    languages = {
+      language = [{
+        name = "markdown";
+        soft-wrap.enable = true;
+        soft-wrap.wrap-indicator = "";
+      }];
+    };
+  };
+}
