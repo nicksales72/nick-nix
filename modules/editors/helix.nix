f4d46513a19e900610cda41ebd424cbf8601ca9f
nicksales7 <nicksales72@gmail.com>
2025-01-16 15:08:48 -0500

    Updated 2025-01-16

--- modules/editors/helix.nix
+++ modules/editors/helix.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }: {
   home.packages = with pkgs; [
     texlab
+    tectonic
   ];
 
   programs.helix = {
@@ -23,13 +24,35 @@
   home.file.".config/helix/languages.toml".text = ''
     [[language]]
     name = "latex"
-    scope = "source.tex"
-    injection-regex = "tex"
-    file-types = ["tex"]
-    roots = []
-    comment-token = "%"
-    language-server = { command = "texlab" }
-    config = { texlab = { build = { onSave = true } } }
-    indent = { tab-width = 4, unit = "\t" }
+
+    [language.config.texlab]
+    auxDirectory = "build"
+
+    [language-server.texlab.config.texlab.chktex]
+    onOpenAndSave = true
+    onEdit = true
+
+    [language-server.texlab.config.texlab.forwardSearch]
+    executable = "zathura"
+    args = [ "--synctex-forward", "%l:%c:%f", "%p" ]
+
+    [language-server.texlab.config.texlab.build]
+    auxDirectory = "build"
+    logDirectory = "build"
+    pdfDirectory = "build"
+
+    forwardSearchAfter = true
+    onSave = true
+
+    executable = "tectonic"
+    args = [
+     "-X",
+     "compile",
+     "--synctex",
+     "--keep-logs",
+     "--keep-intermediates",
+     "--outdir=build",
+     "%f",
+     ]
 '';
 }
