af9c744989a41aa534acd656c1ca5233edbf6776
nicksales7 <nicksales72@gmail.com>
2025-01-24 16:48:01 -0500

    Updated (2025-01-24)

---
+++ modules/editors/helix.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, ... }: {  
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {        
+      theme = "carbonfox";
+      editor = {
+        line-number = "relative";
+        mouse = false;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+      };
+    };
+  };
+}
