1b54408796f107a4b336c3351c4985610876eb85
nicksales7 <nicksales72@gmail.com>
2025-01-26 13:48:17 -0500

    Updated (2025-01-26)

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -2,7 +2,7 @@
   programs.vim = {
     enable = true;
     defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [ vimtex ultisnips markdown-preview-nvim ];
+    plugins = with pkgs.vimPlugins; [ vimtex ultisnips ];
     extraConfig = ''
       syntax on
       colorscheme GruberDarker
