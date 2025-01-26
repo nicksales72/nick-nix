c95f71c8892f2d14924aec9458dcc600ed0ddba1
nicksales7 <nicksales72@gmail.com>
2025-01-26 14:36:29 -0500

    Updated (2025-01-26)

--- imports/user-modules.nix
+++ imports/user-modules.nix
@@ -1,6 +1,7 @@
 { lib, ... }: {
   imports = [
     ../modules/editors/vim.nix
+    ../modules/browsers/librewolf.nix
     ../modules/viewers/zathura.nix
     ../modules/wm/i3blocks.nix
     ../modules/tools/git.nix
---
+++ modules/browsers/librewolf.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, lib, ... }: {
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      "webgl.disabled" = false;
+      "privacy.resistFingerprinting" = false;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+      "network.cookie.lifetimePolicy" = 0;
+    };
+  };
+}
