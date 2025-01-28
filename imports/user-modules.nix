172659f565e1f28462751038b1538b3545129786
nicksales7 <nicksales72@gmail.com>
2025-01-28 16:09:43 -0500

    Updated (2025-01-28)

--- imports/user-modules.nix
+++ imports/user-modules.nix
@@ -2,7 +2,6 @@
   imports = [
     ../modules/editors/vim.nix
     ../modules/editors/ultisnips.nix
-    ../modules/browsers/librewolf.nix
     ../modules/viewers/zathura.nix
     ../modules/wm/i3blocks.nix
     ../modules/tools/git.nix
