95aaa2c34ccb77e04ca844e734374f0c68d87cd3
nicksales7 <nicksales72@gmail.com>
2025-02-09 16:49:26 -0500

    Updated (2025-02-09)

--- modules/users/users.nix
+++ modules/users/users.nix
@@ -2,7 +2,8 @@
   users.users.nick = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "docker" ]; 
-    packages = with pkgs; [ tree feh wget zip unzip vlc clang ghc jdk nodejs R ];
+    packages = with pkgs; [ tree feh wget zip unzip vlc clang 
+                            cmake ghc jdk nodejs R ];
   };
 }
 
