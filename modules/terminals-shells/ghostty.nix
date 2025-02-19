2b930910c2a6fe325e3b25dc31389e685bb28ce8
nicksales72 <nicksales72@gmail.com>
2025-02-19 11:42:16 -0500

    Updated 2025-02-19

--- modules/terminals-shells/ghostty.nix
+++ modules/terminals-shells/ghostty.nix
@@ -3,8 +3,8 @@
     enable = true;
     settings = {
       background = "1C1C1C";
-      cursor-style = "bar";
-      cursor-style-blink = "true";
+      cursor-style = "block";
+      cursor-style-blink = "false";
     };
   };
 }
