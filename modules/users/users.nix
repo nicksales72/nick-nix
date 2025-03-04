71fe94cc6401a2b4a4efba4b2578123029129f28
nicksales72 <nicksales72@gmail.com>
2025-03-04 13:47:23 -0500

    Updated (2025-03-04)

--- modules/users/users.nix
+++ modules/users/users.nix
@@ -3,7 +3,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "docker" ]; 
     packages = with pkgs; [ tree feh wget zip unzip vlc clang helix git
-                            gnumake cmake ghc jdk nodejs R fastfetch ];
+                            gcc gnumake cmake ghc jdk nodejs R fastfetch ];
   };
 }
 
