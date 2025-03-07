ea0a527f01db0fd292f6c32efa022c3084d562d8
nicksales72 <nicksales72@gmail.com>
2025-03-07 16:22:01 -0500

    Updated (2025-03-07)

--- imports/user-modules.nix
+++ imports/user-modules.nix
@@ -1,8 +1,7 @@
 { lib, ... }: {
   imports = [
-    ../modules/editors/helix.nix
+    ../modules/editors/vscode.nix
     ../modules/wm/i3blocks.nix
-    ../modules/tools/zathura.nix
     ../modules/tools/git.nix
     ../modules/themes/gtk-settings.nix
     ../modules/terminals-shells/ghostty.nix
--- modules/users/users.nix
+++ modules/users/users.nix
@@ -2,7 +2,7 @@
   users.users.nick = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "docker" ]; 
-    packages = with pkgs; [ tree feh wget zip unzip vlc clang helix git
+    packages = with pkgs; [ tree feh wget zip unzip vlc clang vim git
                             gcc gnumake cmake ghc jdk nodejs R fastfetch ];
   };
 }
