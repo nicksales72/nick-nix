1a579eb36c62a911b31d46c08523531561e5e41a
nicksales72 <nicksales72@gmail.com>
2025-03-07 16:41:51 -0500

    Updated (2025-03-07)

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -1,5 +1,13 @@
 { config, pkgs, lib, ... }: {
   programs.vscode = {
     enable = true;
+    profiles.default = {
+      userSettings = {
+        "editor.lineNumbers" = "relative"; 
+      };
+      extensions = with pkgs; [
+        vscode-extensions.vscodevim.vim
+      ];
+    };
   };
 }
