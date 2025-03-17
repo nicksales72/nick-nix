71bc915d97506da814af0404bd4f6838782e02db
nicksales72 <nicksales72@gmail.com>
2025-03-17 12:48:18 -0400

    Updated 2025-03-17

--- modules/haskell.nix
+++ modules/haskell.nix
@@ -2,12 +2,6 @@
   environment.systemPackages = with pkgs; [
     cabal-install
     haskell-language-server
-    (haskellPackages.ghcWithPackages (ps: with ps; [
-      regex-base
-      regex-pcre
-      regex-tdfa
-      regex-compat
-    ]))
   ];
 }
 
