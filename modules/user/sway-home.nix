42442dc63550233e45a4a79b9f67c773f8365739
nicksales72 <nicksales72@gmail.com>
2025-03-27 03:05:12 -0400

    Updated 2025-03-27

--- modules/user/sway-home.nix
+++ modules/user/sway-home.nix
@@ -95,6 +95,8 @@
   
   programs.waybar = {
     enable = true;
+    systemd.enable = true;
+    systemd.target = "graphical-session.target";
     settings = {
       mainBar = {
         layer = "top";
