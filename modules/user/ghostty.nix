e2c638ec5ad16df8cc07339d07effd887d9d0e3d
nicksales72 <nicksales72@gmail.com>
2025-04-04 19:03:15 -0400

    Updated 2025-04-04

--- modules/user/ghostty.nix
+++ modules/user/ghostty.nix
@@ -2,7 +2,7 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      background = "1c1c1c";
+      background = "1d2021";
       cursor-style = "block";
       cursor-style-blink = "false";
       shell-integration-features = "no-cursor";
