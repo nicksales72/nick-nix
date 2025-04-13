231e59147c29aef40169a8d3845cb302509d55c2
nicksales72 <nicksales72@gmail.com>
2025-04-13 19:44:38 -0400

    Updated 2025-04-13

--- modules/user/ghostty.nix
+++ modules/user/ghostty.nix
@@ -2,7 +2,7 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      background = "181818";
+      background = "161616";
       cursor-style = "block";
       cursor-style-blink = "false";
       shell-integration-features = "no-cursor";
--- modules/user/tex.nix
+++ modules/user/tex.nix
@@ -1,3 +1,3 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ texlive.combined.scheme-full tectonic ];
+  home.packages = with pkgs; [ texlive.combined.scheme-full ];
 }
