1557263b217dcb5d2f2d560cf593b470c00c0534
nicksales72 <nicksales72@gmail.com>
2025-05-04 18:03:13 -0400

    Updated (2025-05-04)

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }: {
   users.users.kafka = {
     isNormalUser = true;
-    shell = "${pkgs.nushell}/bin/nu";
     extraGroups = [ "wheel" "networkmanager" "docker" "video" ]; 
     packages = with pkgs; [ tree feh wget zip unzip clang helix systemd firefox 
                             mpv git gcc gnumake cmake ghc jdk nodejs R fastfetch ];
