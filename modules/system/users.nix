6b7fa093f980de2c6178a4d51bb2af9b93690328
nicksales72 <nicksales72@gmail.com>
2025-06-23 17:47:40 -0400

    Updated (2025-06-23)

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -2,7 +2,7 @@
   users.users.nick = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "docker" "video" ]; 
-    packages = with pkgs; [ tree feh wget zip unzip clang vim systemd firefox
+    packages = with pkgs; [ tree feh wget zip unzip clang vim systemd firefox google-chrome
                             mpv git gcc gnumake cmake ghc jdk nodejs R fastfetch ];
   };
 }
