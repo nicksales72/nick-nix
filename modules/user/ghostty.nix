337ea68474ac895aac1e0983113ffcaf86d3884f
nicksales72 <nicksales72@gmail.com>
2025-08-28 19:30:16 -0400

    Updated (2025-08-28)

--- modules/user/ghostty.nix
+++ modules/user/ghostty.nix
@@ -3,6 +3,7 @@
     enable = true;
     settings = {
       background = "181818";
+      background-opacity = "0.9";
       cursor-style = "block";
       cursor-style-blink = "false";
       shell-integration-features = "no-cursor";
---
+++ modules/user/helix.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }: {
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "gruber-darker";
+      editor = {
+        line-number = "relative";
+        soft-wrap = {
+          enable = true;
+        };
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+      };      
+      keys.normal = {
+        C-t = ":sh tectonic main.tex";
+      };
+    };
+    extraPackages = with pkgs; [
+      # lsps
+      haskell-language-server
+      jdt-language-server
+      rust-analyzer-unwrapped
+      texlab
+      ruff
+      nil
+    ];
+  };
+}
