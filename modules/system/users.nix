5698f7aa211b4b443e35f1eefb9080182db9a454
nicksales72 <nicksales72@gmail.com>
2025-08-30 13:06:37 -0400

    Updated 2025-08-30

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -3,7 +3,7 @@
     isNormalUser = true;
     shell = "${pkgs.nushell}/bin/nu";
     extraGroups = [ "wheel" "networkmanager" "docker" "video" ];
-    packages = with pkgs; [ tree feh wget zip unzip clang helix systemd brave mpv git gcc gnumake cmake ghc jdk nodejs R fastfetch ];
+    packages = with pkgs; [ tree feh wget zip unzip clang vim systemd brave mpv git gcc gnumake cmake ghc jdk nodejs R fastfetch ];
   };
 }
 
