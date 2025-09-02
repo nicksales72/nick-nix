44b3af60e57539d621bc42b4b86949a6fdb3a225
nicksales72 <nicksales72@gmail.com>
2025-09-02 16:45:31 -0400

    Updated 2025-09-02

--- modules/user/hyprland.nix
+++ modules/user/hyprland.nix
@@ -10,7 +10,7 @@
         touchpad = {
           natural_scroll = true;
           disable_while_typing = true;
-          scroll_factor = 0.2;
+          scroll_factor = 0.1;
         };
       };
 
