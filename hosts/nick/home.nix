833298bb6af5f4774c2255debc9c666a364db84e
nicksales72 <nicksales72@gmail.com>
2025-09-02 15:32:55 -0400

    Updated 2025-09-02

--- hosts/nick/home.nix
+++ hosts/nick/home.nix
@@ -3,11 +3,25 @@
     ../../imports/user-modules.nix
   ];
 
+  programs.home-manager.enable = true;
+
   home.username = "nick";
   home.homeDirectory = "/home/nick";
   home.stateVersion = "25.11";
 
   home.file = { };
+
+  home.pointerCursor = {
+    name = "rose-pine-hyprcursor";
+    package = pkgs.rose-pine-hyprcursor;
+    size = 24;
+    gtk.enable = true;
+
+    hyprcursor = {
+      enable = true;
+      size = 24;
+    };
+  };
 
   home.packages = with pkgs; [
     discord
@@ -22,5 +36,4 @@
     texlive.combined.scheme-full
   ];
 
-  programs.home-manager.enable = true;
 }
