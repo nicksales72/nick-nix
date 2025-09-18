74280b6ec619d95db49b87b833b450ed4e537c0c
nicksales72 <nicksales72@gmail.com>
2025-09-18 16:40:22 -0400

    Updated 2025-09-18

--- modules/user/R.nix
+++ modules/user/R.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 let
   rWithPkgs = pkgs.rWrapper.override {
-    packages = with pkgs.rPackages; [ rmarkdown ];
+    packages = with pkgs.rPackages; [ rmarkdown pandoc ];
   };
 in {
   home.packages = [ rWithPkgs ];
