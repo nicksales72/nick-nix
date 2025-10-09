ae66fcdfd3f69380eb3a7360cdbfd2f96672e26b
nicksales72 <nicksales72@gmail.com>
2025-10-09 17:39:28 -0400

    Updated 2025-10-09

--- modules/user/hyprland.nix
+++ modules/user/hyprland.nix
@@ -56,7 +56,7 @@
         "SUPER, D, exec, rofi -show drun"
 
 	"SUPER_SHIFT, minus, movetoworkspacesilent, special"
-	"SUPER_SHIFT, equals, movetoworkspacesilent, 1"
+	"SUPER_SHIFT, equal, movetoworkspacesilent, 1"
 	"SUPER, minus, togglespecialworkspace"
 
         "SUPER_SHIFT, S, exec, bash -lc 'grim -g \"$(slurp)\" ~/Pictures/$(date +%Y-%m-%d_%H-%M-%S).png'"
